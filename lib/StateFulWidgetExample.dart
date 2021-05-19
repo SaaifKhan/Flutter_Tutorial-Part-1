0e30134272ed47ff5b31747f250ad23d7ba9b38b
saif <saif_khan09@hotmail.com>
2021-05-19 15:42:01 +0500

    Flutter Tutorial 1 basic Login Screen

---
+++ lib/StateFulWidgetExample.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class StateFulWidgetExample extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<StateFulWidgetExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
